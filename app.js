ee1d68b0e08625b3ff0f99fc1f40530a32e23804
DaiMeotti <daianagbm@gmail.com>
2025-02-12 16:34:11 -0300

    Adicionando a lista no HTML

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let listaAmigos = [];
 
+let lista = document.querySelector('ul');
+
+
 function adicionarAmigo() {
     nome = document.querySelector('input').value;   
     
     if (nome === null || nome === undefined || nome === '') {
         alert("Por favor, digite um nome");
+    } else if (listaAmigos.includes(nome)) {
+        alert("Esse nome já está na lista!");
     } else {
         listaAmigos.push(nome);
+        lista.innerHTML = listaAmigos.join('<li>') + '</li>';
         limparCampo();
         return console.log(listaAmigos);
         
@@ -18,5 +24,6 @@ function adicionarAmigo() {
 
 function limparCampo() {
     nome = document.querySelector('input');
-    nome.value = ' ';
+    nome.value = '';
 }
+
